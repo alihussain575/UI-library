d4daa0e5026f4375233755baf8b2ea2283709019
alihussain575 <you@example.com>
2020-12-04 23:30:03 +0500

    this is over first commit

---
+++ taskclass/vue/app.js
@@ -0,0 +1,25 @@
+// new Vue({
+//     el: '#app',
+//     data:{
+//         age:"",
+//         name : "",
+//         x:0,
+//         y:0,
+//     },
+//     methods:{
+//         inputName:function(name){   
+//              console.log(this.name)
+//         },
+//         add:function(inc){
+//             this.age-=inc;
+//         },
+//         subtract:function(dec){
+//             this.age-=dec;
+//         },
+//         updateXY:function(event){
+//             this.x=event.offsetX;
+//             this.y=event.offsetY;
+//         }
+
+//     }
+// })
